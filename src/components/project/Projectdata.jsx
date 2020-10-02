6fc9624c4a39f285efe492a18411db5e8da70663
Lại Minh Sáng <Sanglm2207@gmail.com>
2020-10-02 22:42:16 +0700

    first commit

---
+++ src/components/project/Projectdata.jsx
@@ -0,0 +1,39 @@
+
+import  df  from './a.svg';
+import  df4  from './df4.svg';
+import  df5  from './e.svg';
+
+export const projectdata =  [
+    {
+      info: 'Graduation Thesis at FPT Polytechnic',
+      linkUrl: '',
+      title: 'E-Commerce Website Project',
+      imageUrl: df ,
+      id: 1,
+    },
+    {
+      info: 'Assignment Java 5 at FPT Polytechnic',
+      linkUrl: '',
+      title: 'Employee Management Website Project',
+      imageUrl: df4,
+      id: 2,     
+    },
+    {
+      info: 'Assignment Subject  Project 1 at FPT POlytechnic',
+      linkUrl: '',
+      title: 'Library Management Software Project',
+      imageUrl: df,
+      id: 3,
+    },
+    {
+      info: 'Team Project With F-Code Community Hanoi ',
+      linkUrl: '',
+      title: 'F-Confessions Website Project',
+      imageUrl: df5,
+      id: 4,
+    }
+
+    
+      
+      
+]
---
+++ src/data/skillsData.jsx
@@ -0,0 +1,60 @@
+export const sampleData = [
+    {
+        id: '1',
+        title: 'Java / Swing / JSP Servlet / Struts 2 / Spring Boot',
+        icon: 'fab fa-java fa-spin',
+        percent: 85,
+        barloading: 85
+        
+    },
+    {
+        id: '2',
+        title: 'Node.Js / Express.JS',
+        icon: 'fab fa-node-js fa-spin',
+        percent: 70,
+        barloading: 70
+    },
+    {
+        id: '3',
+        title: 'REST API',
+        icon: 'fas fa-cogs fa-spin',
+        percent: 75,
+        barloading: 75
+    },
+    {
+        id: '4',
+        title: 'Firebase / Firestore / Authentication',
+        icon: 'fab cib-firebase fa-spin',
+        percent: 55,
+        barloading: 55
+    },
+    {
+        id: '5',
+        title: 'Database / MySql / Oracle / SQL Server / Mongodb ',
+        icon: 'fas fa-database fa-spin',
+        percent: 75,
+        barloading: 75
+    },
+    {
+        id: '6',
+        title: 'Angular',
+        icon: 'fab fa-angular fa-spin',
+        percent: 75,
+        barloading: 75
+    },
+    {
+        id: '7',
+        title: 'Git / Github / Gitlab / Npm',
+        icon: 'fab fa-github fa-spin',
+        percent: 90,
+        barloading: 90
+    },
+
+    {
+        id: '8',
+        title: 'Momo Payment',
+        icon: 'far fa-credit-card fa-spin',
+        percent: 30,
+        barloading: 30
+    }
+];
